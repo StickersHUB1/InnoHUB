6dd2cb9a029855566fdb47a5387898d619f68332
Michael StickersHUB1 <163635659+StickersHUB1@users.noreply.github.com>
2025-02-11 13:42:34 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,5 +4,6 @@ const firebaseConfig = {
   projectId: "innohub-376dd",
   storageBucket: "innohub-376dd.appspot.com",
   messagingSenderId: "1022331853512",
-  appId: "1:1022331853512:web:xxxxxxxxxxxxxxx" // Reemplazar "xxxxxxxxxxxxxxx" con el appId real
+  appId: "1:1022331853512:web:
+AIzaSyBk5L8hG3GXANd0uyJUk6VxDU8PWjU5DBk" //
 };
