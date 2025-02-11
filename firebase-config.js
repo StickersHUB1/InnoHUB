c5469fdbed197c5523c262cc5c094aa69fc43779
Michael StickersHUB1 <163635659+StickersHUB1@users.noreply.github.com>
2025-02-11 13:12:18 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBk5L8hG3GXANd0uyJUk6VxDU8PWjU5DBk",
+  authDomain: "innohub-376dd.firebaseapp.com",
+  projectId: "innohub-376dd",
+  storageBucket: "innohub-376dd.appspot.com",
+  messagingSenderId: "1022331853512",
+  appId: "1:1022331853512:web:xxxxxxxxxxxxxxx" // Reemplazar "xxxxxxxxxxxxxxx" con el appId real
+};
